0de6bd504ddcec2cabcab0c7753ea4839c0e0e37
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2019-01-02 15:02:21 +0100

    extract ANSI colors array to separate module

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,15 @@ fn main() {
             "<input_file> 'An input image to display'
             -w, --width=[width] 'Sets width'
             -h, --height=[height] 'Sets height'
-            --true-colour 'Uses 24-bit colour palette'",
+            --true-color 'Uses 24-bit color palette'",
         )
         .get_matches();
 
     let file_name = matches.value_of("input_file").unwrap();
     let width = value_t!(matches, "width", u32);
     let height = value_t!(matches, "height", u32);
-    let true_colour = matches.is_present("true-colour");
+    let true_color = matches.is_present("true-color");
 
-    let args = Args::new(file_name, width, height, true_colour);
-
+    let args = Args::new(file_name, width, height, true_color);
     terminal_image::run(args);
 }
